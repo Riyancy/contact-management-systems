a303234e04aee0179456a624b12fb8d11943b450
Ithayathas Merio Riyancy <ithayathasriyancy68ir02@gmail.com>
2026-05-10 13:42:29 +0530

    updated POST API success and message

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,6 +1,20 @@
 import Contact from "../models/Contact.js";
 
 export const createContact = async (req, res) => {
+  try {
     const contact = await Contact.create(req.body);
-    res.json(contact);
+
+    res.status(201).json({
+      success: true,
+      message: "Contact created successfully",
+      data: contact
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Failed to create contact",
+      error: error.message
+    });
+  }
 };
